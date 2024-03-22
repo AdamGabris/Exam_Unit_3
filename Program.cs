cd603962f6c90e7037d301c040b3b833c18bd27f
Adam Gábriš <adamos.gabris@gmail.com>
2024-03-22 15:21:40 +0100

    First solutions for Task 4, still with minor bugs

--- Program.cs
+++ Program.cs
@@ -104,6 +104,11 @@ void ReadBooksFromFile(string filename)
     List<Book> booksStartingWithThe = GetBooksStartingWithThe(books);
     List<Book> booksOfAuthorsWithT = GetBooksOfAuthorsWithT(books);
     List<Book> booksWrittenAfter = GetBooksWrittenAfterOrBefore(books, 2004, false);
+    List<string> isbnForAuthor = GetISBNForAuthor(books, "Terry Pratchett");
+    List<Book> booksAlphabetically = ListAllBooksAlphabetically(books, true);
+    List<Book> booksChronologically = ListAllBooksChronologically(books, true);
+    List<Book> booksGroupedByLastName = GroupBooksByLastName(books);
+    List<Book> booksGroupedByFirstName = GroupBooksByFirstName(books);
 
 
     /*  foreach (var book in books)
@@ -121,10 +126,32 @@ void ReadBooksFromFile(string filename)
             Console.WriteLine($"Title: {book.title}, Author: {book.author}, Publication Year: {book.publication_year}, ISBN: {book.isbn}");
         } */
 
-    foreach (var book in booksWrittenAfter)
+    /*     foreach (var book in booksWrittenAfter)
+        {
+            Console.WriteLine($"Title: {book.title}, Author: {book.author}, Publication Year: {book.publication_year}, ISBN: {book.isbn}");
+        } */
+    /*    foreach (var isbn in isbnForAuthor)
+       {
+           Console.WriteLine(isbn);
+       } */
+    /*     foreach (var book in booksAlphabetically)
+        {
+            Console.WriteLine($"Title: {book.title}, Author: {book.author}, Publication Year: {book.publication_year}, ISBN: {book.isbn}");
+        } */
+
+    /*     foreach (var book in booksChronologically)
+        {
+            Console.WriteLine($"Title: {book.title}, Author: {book.author}, Publication Year: {book.publication_year}, ISBN: {book.isbn}");
+        } */
+    /*     foreach (var book in booksGroupedByLastName)
+        {
+            Console.WriteLine($"Title: {book.title}, Author: {book.author}, Publication Year: {book.publication_year}, ISBN: {book.isbn}");
+        } */
+    foreach (var book in booksGroupedByFirstName)
     {
         Console.WriteLine($"Title: {book.title}, Author: {book.author}, Publication Year: {book.publication_year}, ISBN: {book.isbn}");
     }
+
 }
 
 List<Book> GetBooksStartingWithThe(List<Book> books)
@@ -160,7 +187,8 @@ List<Book> GetBooksOfAuthorsWithT(List<Book> books)
     foreach (var book in books)
     {
         bool foundT = false;
-        foreach (char c in book.author)
+        string authorName = book.author.Split(new string[] { "(Translated by" }, StringSplitOptions.None)[0];
+        foreach (char c in authorName)
         {
             if (c == 't' || c == 'T')
             {
@@ -199,6 +227,147 @@ List<Book> GetBooksWrittenAfterOrBefore(List<Book> books, int year, bool after)
     return result;
 }
 
+List<string> GetISBNForAuthor(List<Book> books, string author)
+{
+    List<string> result = new List<string>();
+    foreach (var book in books)
+    {
+        string authorName = book.author.Split(new string[] { "(Translated by" }, StringSplitOptions.None)[0].Trim();
+        if (authorName == author)
+        {
+            result.Add(book.isbn);
+        }
+    }
+    return result;
+}
+
+List<string> GetAuthorLastName(List<Book> books)
+{
+    List<string> result = new List<string>();
+    foreach (var book in books)
+    {
+        string authorName = book.author.Split(new string[] { "(Translated by" }, StringSplitOptions.None)[0].Trim();
+        string[] names = authorName.Split(' ');
+        result.Add(names[names.Length - 1]);
+    }
+    return result;
+}
+
+List<string> GetAuthorFirstName(List<Book> books)
+{
+    List<string> result = new List<string>();
+    foreach (var book in books)
+    {
+        string authorName = book.author.Split(new string[] { "(Translated by" }, StringSplitOptions.None)[0].Trim();
+        string[] names = authorName.Split(' ');
+        result.Add(names[0]);
+    }
+    return result;
+}
+
+List<Book> ListAllBooksAlphabetically(List<Book> books, bool ascending)
+{
+    Book temporary;
+    for (int i = 0; i < books.Count - 1; i++)
+    {
+        for (int j = i + 1; j < books.Count; j++)
+        {
+            if (ascending)
+            {
+                if (string.Compare(books[i].title, books[j].title) > 0)
+                {
+                    temporary = books[i];
+                    books[i] = books[j];
+                    books[j] = temporary;
+                }
+            }
+            else
+            {
+                if (string.Compare(books[i].title, books[j].title) < 0)
+                {
+                    temporary = books[i];
+                    books[i] = books[j];
+                    books[j] = temporary;
+                }
+            }
+        }
+    }
+    return books;
+}
+
+List<Book> ListAllBooksChronologically(List<Book> books, bool ascending)
+{
+    Book temporary;
+    for (int i = 0; i < books.Count - 1; i++)
+    {
+        for (int j = i + 1; j < books.Count; j++)
+        {
+            if (ascending)
+            {
+                if (books[i].publication_year > books[j].publication_year)
+                {
+                    temporary = books[i];
+                    books[i] = books[j];
+                    books[j] = temporary;
+                }
+            }
+            else
+            {
+                if (books[i].publication_year < books[j].publication_year)
+                {
+                    temporary = books[i];
+                    books[i] = books[j];
+                    books[j] = temporary;
+                }
+            }
+        }
+    }
+    return books;
+}
+
+List<Book> GroupBooksByLastName(List<Book> books)
+{
+    List<Book> result = new List<Book>();
+    List<string> lastNames = GetAuthorLastName(books);
+    List<string> firstNames = GetAuthorFirstName(books);
+    for (int i = 0; i < lastNames.Count; i++)
+    {
+        for (int j = i + 1; j < lastNames.Count; j++)
+        {
+            if (lastNames[i] == lastNames[j])
+            {
+                if (firstNames[i] == firstNames[j])
+                {
+                    result.Add(books[i]);
+                    result.Add(books[j]);
+                }
+            }
+        }
+    }
+    return result;
+}
+
+List<Book> GroupBooksByFirstName(List<Book> books)
+{
+    List<Book> result = new List<Book>();
+    List<string> lastNames = GetAuthorLastName(books);
+    List<string> firstNames = GetAuthorFirstName(books);
+    for (int i = 0; i < lastNames.Count; i++)
+    {
+        for (int j = i + 1; j < lastNames.Count; j++)
+        {
+            if (firstNames[i] == firstNames[j])
+            {
+                if (lastNames[i] == lastNames[j])
+                {
+                    result.Add(books[i]);
+                    result.Add(books[j]);
+                }
+            }
+        }
+    }
+    return result;
+}
 
 public class Book
 {
